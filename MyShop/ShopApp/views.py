2d5ac8d37c7ace2b218648a28b822a155b6ef4a0
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-09-09 20:54:16 +0600

    Dynamic

--- MyShop/ShopApp/views.py
+++ MyShop/ShopApp/views.py
@@ -5,10 +5,17 @@ def this_shop(request):
     return HttpResponse("Welcome to our shop!!!")
 
 def homepage(request):
-    return render(request,"index.html")
+    page={"home":"Home Page","content":"A shop , Thousands of happiness"}
+    return render(request,"index.html",page)
 
 def contact(request):
-    return render(request,"contact.html")
+    email="contact@example.com",
+    social_profiles=[
+        "Facebook: fb.m\example",
+        "Twitter: hello.\tweet",
+        "LinedIN: nowshin.\linkedin"
+    ]
+    return render(request,"contact.html",{"emailaddress":email},{"socialprofiles":social_profiles})
 
 def about(request):
     return render(request,"about.html")
